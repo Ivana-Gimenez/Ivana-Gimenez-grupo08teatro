d3d6bdc2b1e9d9fbc24a0c9e900e4c69e8df45c7
Natalia Librada Benitez <nataliabenitez237@gmail.com>
2026-05-15 16:13:19 -0300

    fix: corregir modelo Rol con tabla 'roles' y DatabaseSeeder

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Rol extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'roles';
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'rol_id');
+    }
 }
